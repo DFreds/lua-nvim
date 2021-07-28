0c87810e5083a60a89369eb8c087511389c1e59e
Derek Fredrickson <fredricksond@upmc.edu>
2021-07-27 20:39:26 -0400

    Implementation of peek definition

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -12,9 +12,10 @@ local on_attach = function(client, bufnr)
       { '<leader>j', '<Cmd>lua vim.lsp.buf.code_action()<CR>' },
       { 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>' },
       { 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>' },
-      { '<S-k>', '<Cmd>lua vim.lsp.buf.hover()<CR>' },
       { 'gi', '<Cmd>lua vim.lsp.buf.implementation()<CR>' },
       { 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>' },
+      { 'gp', "<cmd>lua require 'lsp-config'.PeekDefinition()<cr>" },
+      { '<S-k>', '<Cmd>lua vim.lsp.buf.hover()<CR>' },
       { '<leader>rn', '<Cmd>lua vim.lsp.buf.rename()<CR>' },
       { '<leader>d', '<cmd>lua vim.lsp.diagnostic.set_loclist()<cr>' },
       { '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>' },
@@ -102,6 +103,73 @@ local on_attach = function(client, bufnr)
   end
 end
 
+local lsp_config = {}
+
+-- Taken from https://www.reddit.com/r/neovim/comments/gyb077/nvimlsp_peek_defination_javascript_ttserver/
+function lsp_config.preview_location(location, context, before_context)
+  -- location may be LocationLink or Location (more useful for the former)
+  context = context or 15
+  before_context = before_context or 0
+  local uri = location.targetUri or location.uri
+  if uri == nil then
+    return
+  end
+  local bufnr = vim.uri_to_bufnr(uri)
+  if not vim.api.nvim_buf_is_loaded(bufnr) then
+    vim.fn.bufload(bufnr)
+  end
+
+  local range = location.targetRange or location.range
+  local contents = vim.api.nvim_buf_get_lines(
+    bufnr,
+    range.start.line - before_context,
+    range["end"].line + 1 + context,
+    false
+  )
+  local filetype = vim.api.nvim_buf_get_option(bufnr, "filetype")
+  return vim.lsp.util.open_floating_preview(contents, filetype, { border = "single" })
+end
+
+function lsp_config.preview_location_callback(_, method, result)
+  local context = 15
+  if result == nil or vim.tbl_isempty(result) then
+    print("No location found: " .. method)
+    return nil
+  end
+  if vim.tbl_islist(result) then
+    lsp_config.floating_buf, lsp_config.floating_win = lsp_config.preview_location(result[1], context)
+  else
+    lsp_config.floating_buf, lsp_config.floating_win = lsp_config.preview_location(result, context)
+  end
+end
+
+function lsp_config.PeekDefinition()
+  if vim.tbl_contains(vim.api.nvim_list_wins(), lsp_config.floating_win) then
+    vim.api.nvim_set_current_win(lsp_config.floating_win)
+  else
+    local params = vim.lsp.util.make_position_params()
+    return vim.lsp.buf_request(0, "textDocument/definition", params, lsp_config.preview_location_callback)
+  end
+end
+
+--[[ function lsp_config.PeekTypeDefinition()
+  if vim.tbl_contains(vim.api.nvim_list_wins(), lsp_config.floating_win) then
+    vim.api.nvim_set_current_win(lsp_config.floating_win)
+  else
+    local params = vim.lsp.util.make_position_params()
+    return vim.lsp.buf_request(0, "textDocument/typeDefinition", params, lsp_config.preview_location_callback)
+  end
+end
+
+function lsp_config.PeekImplementation()
+  if vim.tbl_contains(vim.api.nvim_list_wins(), lsp_config.floating_win) then
+    vim.api.nvim_set_current_win(lsp_config.floating_win)
+  else
+    local params = vim.lsp.util.make_position_params()
+    return vim.lsp.buf_request(0, "textDocument/implementation", params, lsp_config.preview_location_callback)
+  end
+end ]]
+
 -- Configure lua language server for neovim development
 local lua_settings = {
   Lua = {
@@ -169,3 +237,5 @@ require 'lspinstall'.post_install_hook = function ()
   setup_servers() -- reload installed servers
   -- vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
 end
+
+return lsp_config
