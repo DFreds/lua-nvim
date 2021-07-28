126bd4f639cfff3a376f23663a39835a89de9c1c
Derek Fredrickson <fredricksond@upmc.edu>
2021-07-27 20:33:56 -0400

    Configure lsp with keybindings and custom views

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -9,13 +9,14 @@ local on_attach = function(client, bufnr)
   -- Mappings.
   local lsp_mappings = {
     n = { -- normal mode
+      { '<leader>j', '<Cmd>lua vim.lsp.buf.code_action()<CR>' },
       { 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>' },
       { 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>' },
       { '<S-k>', '<Cmd>lua vim.lsp.buf.hover()<CR>' },
       { 'gi', '<Cmd>lua vim.lsp.buf.implementation()<CR>' },
       { 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>' },
       { '<leader>rn', '<Cmd>lua vim.lsp.buf.rename()<CR>' },
-      { '<leader>d', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>' },
+      { '<leader>d', '<cmd>lua vim.lsp.diagnostic.set_loclist()<cr>' },
       { '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>' },
       { ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>' },
     }
@@ -23,14 +24,64 @@ local on_attach = function(client, bufnr)
 
   utils.register_mappings(lsp_mappings)
 
+  -- Set Default Prefix.
+  vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+    virtual_text = {
+      prefix = "",
+      spacing = 0,
+    },
+    signs = true,
+    underline = true,
+  })
+
+  -- Add border to hover
+  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+    border = "single",
+  })
+
+  -- Add border to signature help
+  vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+    border = "single",
+  })
+
+  -- set completion item kinds
+  vim.lsp.protocol.CompletionItemKind = {
+    "   (Text) ",
+    "   (Method)",
+    "   (Function)",
+    "   (Constructor)",
+    " ﴲ  (Field)",
+    "[] (Variable)",
+    "   (Class)",
+    " ﰮ  (Interface)",
+    "   (Module)",
+    " 襁 (Property)",
+    "   (Unit)",
+    "   (Value)",
+    " 練 (Enum)",
+    "   (Keyword)",
+    "   (Snippet)",
+    "   (Color)",
+    "   (File)",
+    "   (Reference)",
+    "   (Folder)",
+    "   (EnumMember)",
+    " ﲀ  (Constant)",
+    " ﳤ  (Struct)",
+    "   (Event)",
+    "   (Operator)",
+    "   (TypeParameter)",
+  }
+
   -- local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
   -- local opts = { noremap=true, silent=true }
   -- buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
   -- buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
   -- buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
   -- buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+
   -- buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  --buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  -- buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
 
   -- Set some keybinds conditional on server capabilities
   --[[ if client.resolved_capabilities.document_formatting then
