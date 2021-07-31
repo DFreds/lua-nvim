06668bbde08c628d24c6f2613cf9e588c4d62742
Derek Fredrickson <fredricksond@upmc.edu>
2021-07-31 17:36:55 -0400

    Show relative file path in lualine

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -9,7 +9,12 @@ require "lualine" .setup {
   sections = {
     lualine_a = {"mode"},
     lualine_b = {"branch"},
-    lualine_c = {"filename"},
+    lualine_c = {
+      {
+        "filename",
+        path = 1, -- relative path
+      },
+    },
     lualine_x = {"encoding", "fileformat", "filetype"},
     lualine_y = {"progress"},
     lualine_z = {"location"}
